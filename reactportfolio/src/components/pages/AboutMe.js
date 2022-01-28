c4f8a89b33231279620ad4a4122ac4950a47fb2f
Justin Houk <jstndhouk@gmail.com>
2022-01-28 17:55:55 -0500

    changed content from the activity to my header labels, content etc

---
+++ reactportfolio/src/components/pages/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+      I'm Justin Houk and I am an eager full stack developer and am currently enrolled in Georgia Tech's Coding bootcamp slated to finish in February 2022.
+My approach to software development is 'creating the most simple solution for the most difficult problems'. A good software solution is concise, easy to interpret, and efficient.
+I have spent most of my professional career in the construction management world. Coming from an engineering background, development is a fun and engaging skill to get into and I quickly learned to love the process of programming.
+Take a look at some of my most interesting projects below. I had a lot of fun with these! Feel free to play around with them and let me know if you have any questions. JustFurPets and Lazy-Layover were the biggest projects that I have put together and are particularly special because I had a hand in the design, idea, and functionality. My contact links are at the bottom of this page. Thanks for visiting my portfolio!  Check out my work on my Github!  The links to all my platforms are below.
+      </p>
+    </div>
+  );
+}
